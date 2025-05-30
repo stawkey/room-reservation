8a12bd79fc54ac6674f04e1637695f17c39f0964
stawkey <82365208+stawkey@users.noreply.github.com>
2025-05-30 20:45:11 +0200

    Initial commit

--- src/main/java/org/openapitools/OpenApiGeneratorApplication.java
+++ src/main/java/org/openapitools/OpenApiGeneratorApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
 @SpringBootApplication(
     nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
 )
 @ComponentScan(
-    basePackages = {"org.openapitools", "io.github.stawkey.api" , "org.openapitools.configuration"},
+    basePackages = {"org.openapitools", "io.github.stawkey.roomreservation.api",
+            "io.github.stawkey.roomreservation.controller" , "org.openapitools.configuration"},
     nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
 )
 public class OpenApiGeneratorApplication {
